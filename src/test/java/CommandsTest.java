02105356c20661d3af689b656ab8ce9ff38c9082
Sergo Amariani <sergoamariani@gmail.com>
2021-07-22 13:48:22 +0400

    refactor: add comments to the code

--- src/test/java/CommandsTest.java
+++ src/test/java/CommandsTest.java
@@ -18,16 +18,16 @@ public class CommandsTest {
         WebDriver driver = new ChromeDriver();
 
         driver.navigate().to("http://the-internet.herokuapp.com/dynamic_controls");
-
+        //clicking on button "Enable"
         WebElement btnEnable = driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/form[2]/button"));
         btnEnable.click();
         // waiting 7 seconds after clicking the button so that the input field can turn on
         WebDriverWait wait = new WebDriverWait(driver, 7);
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[2]/div/div[1]/form[2]/button")));
-
+        //checking that input field is enabled
         WebElement input = driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/form[2]/input"));
         System.out.println("\"input field is enabled\": " + input.isEnabled());
-
+        //checking that text 'It's enabled!' is displayed
         WebElement msgDisplayed = driver.findElement(By.xpath("/html/body/div[2]/div/div[1]/form[2]/p"));
         System.out.println("\"text \"It's enabled!\"is displayed\":" + msgDisplayed.isDisplayed());
 
@@ -36,7 +36,7 @@ public class CommandsTest {
             System.out.println("Button text is changed to:" + btnEnable.getText());
         }else
             System.out.println("Button text is changed to:" + btnEnable.getText());
-
+        //writing "Bootcamp" in  input field and clear it
         input.sendKeys("Bootcamp");
         input.clear();
     }
@@ -46,7 +46,7 @@ public class CommandsTest {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.navigate().to("http://the-internet.herokuapp.com/drag_and_drop");
-
+        //Checking that Y coordinate of column A and column B are the same
         int element_a= driver.findElement(By.id("column-a")).getLocation().getY();
         int element_b= driver.findElement(By.id("column-b")).getLocation().getY();
         System.out.println("Column A y.coordinate:"+element_a+ "    Column B y.coordinate: "+element_b);
