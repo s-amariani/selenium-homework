f427d96949e12d49af8d4bf2e93543435a4b7471
Sergo Amariani <sergoamariani@gmail.com>
2021-08-01 15:45:22 +0400

    refactor: run all test in headless mode

--- src/test/java/FileUploadTest.java
+++ src/test/java/FileUploadTest.java
@@ -1,17 +1,22 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.testng.annotations.Test;
 
 public class FileUploadTest {
 
     @Test
     public void FileUpload() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
+        // WebDriverManager.chromedriver().setup();
+        //WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME,true); //Run  test in headless mode
 
         String mainPageUrl = "http://the-internet.herokuapp.com/upload";
         driver.navigate().to(mainPageUrl);
